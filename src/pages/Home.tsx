797308be7a23071c2f770fade0668e7ee2c0a33f
NikitaA-12 <abramov10nikita12@gmail.com>
2023-07-13 15:09:23 +0700

    first-load

---
+++ src/pages/Home.tsx
@@ -0,0 +1,105 @@
+import React from 'react';
+import qs from 'qs';
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+
+import { Categories, Sort, PizzaBlock, Skeleton, Pagination } from '../components';
+
+import { sortList } from '../components/Sort';
+
+import { useAppDispatch } from '../redux/store';
+import { selectFilter } from '../redux/filter/selectors';
+import { selectPizzaData } from '../redux/pizza/selectors';
+import { setCategoryId, setCurrentPage } from '../redux/filter/slice';
+import { fetchPizzas } from '../redux/pizza/asyncActions';
+import { SearchPizzaParams } from '../redux/pizza/types';
+
+const Home: React.FC = () => {
+  const navigate = useNavigate();
+  const dispatch = useAppDispatch();
+  const isMounted = React.useRef(false);
+  const { items, status } = useSelector(selectPizzaData);
+  const { categoryId, sort, currentPage, searchValue } = useSelector(selectFilter);
+
+  const onChangeCategory = React.useCallback((idx: number) => {
+    dispatch(setCategoryId(idx));
+  }, []);
+
+  const onChangePage = (page: number) => {
+    dispatch(setCurrentPage(page));
+  };
+
+  const getPizzas = async () => {
+    const sortBy = sort.sortProperty.replace('-', '');
+    const order = sort.sortProperty.includes('-') ? 'asc' : 'desc';
+    const category = categoryId > 0 ? `category=${categoryId}` : '';
+    const search = searchValue ? `&search=${searchValue}` : '';
+
+    dispatch(
+      fetchPizzas({
+        sortBy,
+        order,
+        category,
+        search,
+        currentPage: String(currentPage),
+      }),
+    );
+
+    window.scrollTo(0, 0);
+  };
+
+  React.useEffect(() => {
+    if (isMounted.current) {
+      const params = {
+        categoryId: categoryId > 0 ? categoryId : null,
+        sortProperty: sort.sortProperty,
+        currentPage,
+      };
+      const queryString = qs.stringify(params, { skipNulls: true });
+
+      navigate(`/?${queryString}`);
+    }
+    if (!window.location.search) {
+      dispatch(fetchPizzas({} as SearchPizzaParams));
+    }
+  }, [categoryId, sort.sortProperty, searchValue, currentPage]);
+
+  React.useEffect(() => {
+    getPizzas();
+  }, [categoryId, sort.sortProperty, searchValue, currentPage]);
+
+  // //React.useEffect(() => {
+  //   window.scrollTo(0, 0);
+
+  //  // if (!isSearch.current) {
+  //     getPizzas();
+  //   }
+
+  // //  isSearch.current = false;
+  // }, []);
+
+  const pizzas = items.map((obj: any) => <PizzaBlock key={obj.id} {...obj} />);
+
+  const skeletons = [...new Array(6)].map((_, index) => <Skeleton key={index} />);
+
+  return (
+    <div className="container">
+      <div className="content__top">
+        <Categories value={categoryId} onChangeCategory={onChangeCategory} />
+        <Sort value={sort} />
+      </div>
+      <h2 className="content__title">Все пиццы</h2>
+      {status === 'error' ? (
+        <div className="content__error-info">
+          <h2>Произошла ошибка 😕</h2>
+          <p>Не удалось получить данные</p>
+        </div>
+      ) : (
+        <div className="content__items">{status === 'loading' ? skeletons : pizzas}</div>
+      )}
+      <Pagination currentPage={currentPage} onChangePage={onChangePage} />
+    </div>
+  );
+};
+
+export default Home;
